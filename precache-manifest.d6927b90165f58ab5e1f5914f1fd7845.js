c7983925d74d1dd41966dcaf0d1898465cf0ed73
zcrutcher <50390021+zcrutcher@users.noreply.github.com>
2021-10-15 22:36:29 -0500

    Updates

---
+++ precache-manifest.d6927b90165f58ab5e1f5914f1fd7845.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8a9d77b13ef99e934edaa020d366e91",
+    "url": "/elm-toDoV2/elm.js"
+  },
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/elm-toDoV2/favicon.ico"
+  },
+  {
+    "revision": "0c141b91e3218a13770b5578f00376d6",
+    "url": "/elm-toDoV2/icons/check.svg"
+  },
+  {
+    "revision": "ccfcce4898b4444ea97ba70bf56d1241",
+    "url": "/elm-toDoV2/icons/plus-black-symbol.svg"
+  },
+  {
+    "revision": "2e2cad19c23737d70c5ad33e5d4db1c3",
+    "url": "/elm-toDoV2/icons/trash.svg"
+  },
+  {
+    "revision": "32c69109e507cbe556342c1aa7eef211",
+    "url": "/elm-toDoV2/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/elm-toDoV2/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/elm-toDoV2/manifest.json"
+  },
+  {
+    "revision": "50e66b67741aa6532267",
+    "url": "/elm-toDoV2/static/css/main.ba0e5ff8.chunk.css"
+  },
+  {
+    "revision": "50e66b67741aa6532267",
+    "url": "/elm-toDoV2/static/js/main.50e66b67.chunk.js"
+  },
+  {
+    "revision": "571c8fa95cca0f032da8",
+    "url": "/elm-toDoV2/static/js/runtime~main.571c8fa9.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/elm-toDoV2/static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
